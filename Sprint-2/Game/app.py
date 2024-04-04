11446ac6d407df9c5aefb5455dc1a975fe360d5d
AldredS <saldred1@umbc.edu>
2024-04-04 15:37:37 -0400

    Made a working button - Can click on green text and open the screen

--- Sprint-2/Game/app.py
+++ Sprint-2/Game/app.py
@@ -6,7 +6,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def display():
-    return render_template('part9.html')
+    return render_template('combat.html')
 
 if __name__ == "__main__":
     app.run(debug=True)
