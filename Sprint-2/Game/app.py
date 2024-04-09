cb1f5c558406d4b4851d955b853c25dfab335cf3
Bhojraj123 <pandeybhojraj27@gmail.com>
2024-04-09 16:23:14 -0400

    updating the app.py

--- Sprint-2/Game/app.py
+++ Sprint-2/Game/app.py
@@ -3,10 +3,17 @@ import sqlite3
 
 app = Flask(__name__)
 
+def get_db_connection():
+    conn = sqlite3.connect('game.db')
+    conn.row_factory = sqlite3.Row  # This enables column access by name: row['column_name']
+    return conn
 
 @app.route("/")
 def display():
-    return render_template('part9.html')
+    conn = get_db_connection()
+    players = conn.execute('SELECT * FROM player_scores ORDER BY score DESC').fetchall()
+    conn.close()
+    return render_template('mainMenu.html', players=players)
 
 if __name__ == "__main__":
     app.run(debug=True)
