44b7276188cf94fb354131dd3c1dae835b6667ed
PeterGQ <pandeve3@gmail.com>
2024-04-09 21:41:38 -0400

    sprint 2 push

--- Sprint-2/Game/app.py
+++ Sprint-2/Game/app.py
@@ -9,7 +9,7 @@ def index():
     # Connect to the SQLite database
     conn = sqlite3.connect('items.db')
     cursor = conn.cursor()
-    return render_template('part9.html')
+    return render_template('map.html')
     # Example query to retrieve player information
     cursor.execute("SELECT username,player_healh, player_currency, player_atk, player_def FROM logins WHERE userID = ?", (1,))
     player_data = cursor.fetchone()  # Assuming you only expect one row
@@ -17,7 +17,7 @@ def index():
     conn.close()
 
     # Pass player data to the HTML template
-    return render_template('part9.html', player_data=player_data)
+    return render_template('map.html', player_data=player_data)
 
 
 # add a border to the map, should cover the watermark and include the title of the game
